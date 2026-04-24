411dcd5b27a0e90b2ff3615d3a91210c27b5e458
Sebastian Torres <97627021+Jughead6@users.noreply.github.com>
2026-04-24 16:21:53 -0500

    feat: db connection url

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,12 +5,23 @@ dotenv.config()
 
 const { Pool } = pkg
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+const isProduction = process.env.NODE_ENV === "production"
+
+const pool = new Pool(
+    isProduction
+        ? {
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        }
+        : {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        }
+)
 
 export default pool
